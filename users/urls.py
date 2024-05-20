62146da0ef11cc244bfd7ffb09b088820e0516f7
bargest <DIZz92@mail.ru>
2024-05-20 16:18:13 +0300

    add 25.1

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from users.apps import UsersConfig
 from django.urls import path
-from users.views import UserCreateAPIView,UserRetrieveAPIView,UserUpdateAPIView,UserListAPIView,UserDestroyAPIView,PaymentListAPIView,PaymentCreateAPIView
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from users.views import UserCreateAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserListAPIView, UserDestroyAPIView, \
+    PaymentListAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 app_name = UsersConfig.name
 
 urlpatterns = [
@@ -10,6 +12,10 @@ urlpatterns = [
     path('detail/<int:pk>', UserRetrieveAPIView.as_view(), name='user_detail'),
     path('edit/<int:pk>', UserUpdateAPIView.as_view(), name='user_edit'),
     path('delete/<int:pk>', UserDestroyAPIView.as_view(), name='user_delete'),
+
     path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
-    path('payment/create', PaymentCreateAPIView.as_view(), name='payment_create'),
+    #path('payment/create', PaymentCreateAPIView.as_view(), name='payment_create'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django_filters import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from materials.permissions import IsAuth
 
 from users.serializers import UserSerializer, PaymentSerializer
 from users.models import User, Payment
@@ -10,21 +11,25 @@ from users.models import User, Payment
 
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 
 class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuth]
 
 
 class UserDestroyAPIView(generics.DestroyAPIView):
@@ -37,6 +42,7 @@ class PaymentListAPIView(generics.ListAPIView):
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_fields = ('course_paid', 'subject_paid', 'payment_method')
     ordering_fields = ('payment_date',)
+    permission_classes = [IsAuthenticated]
 
 
 
